34eeaff3f008555a7735846b3e61520dacc47b81
pakoito <jueces.jus@gmail.com>
2019-10-09 10:45:50 +0100

    Add lib to cargo

--- target/package/ggwp-0.1.0/target/debug/deps/ggwp-c442c950268a8768.d
+++
@@ -1,5 +0,0 @@
-/Users/paco/Coding/Rust/ggwp/target/package/ggwp-0.1.0/target/debug/deps/ggwp-c442c950268a8768: src/main.rs
-
-/Users/paco/Coding/Rust/ggwp/target/package/ggwp-0.1.0/target/debug/deps/ggwp-c442c950268a8768.d: src/main.rs
-
-src/main.rs:
--- target/package/ggwp-0.1.0/target/debug/ggwp.d
+++
@@ -1,1 +0,0 @@
-/Users/paco/Coding/Rust/ggwp/target/package/ggwp-0.1.0/target/debug/ggwp: /Users/paco/Coding/Rust/ggwp/target/package/ggwp-0.1.0/src/main.rs
