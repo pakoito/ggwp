095a6ba43610870f64f24d45f22288546625dd5d
pakoito <jueces.jus@gmail.com>
2020-09-26 20:35:22 +0200

    Some of synctest

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,307 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(non_camel_case_types)]
+use ringbuf::RingBuffer;
 use std::any::Any;
+use std::cell::{Cell, RefCell};
+use std::path::Path;
 
-TODO synctest.cpp
+continue synctest.cpp
+sync.h
+sync.cpp
 
 #[test]
 fn main_test() {
     println!("PATATAS {}", ggpo_succeeded(GGPO_OK));
 }
+
+/////////////////////////
+/// SyncTest
+/////////////////////////
+
+const GAMEINPUT_MAX_BYTES: usize = 9;
+const GAMEINPUT_MAX_PLAYERS: usize = 2;
+
+#[derive(Default)]
+struct SavedInfo {
+    frame: i32,
+    checksum: i32,
+    buf: Vec<u8>,
+    cbuf: usize,
+    input: GameInput,
+}
+
+#[derive(Default, Clone)]
+struct GameInput {
+    frame: Option<i32>,
+    size: usize,
+    bits: Cell<[u8; GAMEINPUT_MAX_BYTES * GAMEINPUT_MAX_PLAYERS]>,
+}
+
+impl GameInput {
+    fn erase(&mut self) {
+        self.bits
+            .set([0; GAMEINPUT_MAX_BYTES * GAMEINPUT_MAX_PLAYERS])
+    }
+}
+
+struct GGPOSync;
+
+impl GGPOSync {
+    fn save_current_frame(&self) {}
+    fn get_frame_count(&self) -> i32 {
+        0
+    }
+    fn increment_frame(&self) {}
+
+    fn get_last_saved_frame(&self) -> &SavedInfo {
+        todo!();
+    }
+
+    fn load_frame(&self, frame: i32) {}
+}
+
+struct SyncTestSession {
+    cb: Box<dyn GGPOSessionCallbacks>,
+    sync: GGPOSync,
+    num_players: i32,
+    rollingback: Cell<bool>,
+    running: Cell<bool>,
+    logfp: Option<Box<Path>>,
+    game: String,
+    check_distance: i32,
+    last_verified: Cell<i32>,
+    current_input: Cell<GameInput>,
+    last_input: Cell<GameInput>,
+    saved_frames: RefCell<RingBuffer<SavedInfo>>,
+}
+
+impl Drop for SyncTestSession {
+    fn drop(&mut self) {}
+}
+
+impl opaque::Opaque for SyncTestSession {
+    fn do_poll(&self, timeout: i32) -> GGPOErrorCode {
+        if !self.running.get() {
+            let info = GGPOEvent {
+                code: GGPOEventCode::GGPO_EVENTCODE_RUNNING,
+                u: GGPOEventValue::GGPO_EVENTCODE_RUNNING,
+            };
+            self.cb.on_event(&info);
+            self.running.set(true);
+        }
+        GGPO_OK
+    }
+
+    fn add_player(
+        &self,
+        player: &GGPOPlayer,
+        handle: &GGPOPlayerHandle,
+    ) -> GGPOResult<GGPOPlayerHandle> {
+        if player.player_num < 1 || player.player_num > self.num_players {
+            return Err(GGPOErrorCode::GGPO_ERRORCODE_PLAYER_OUT_OF_RANGE);
+        }
+        Ok(player.player_num - 1)
+    }
+
+    fn add_local_input(
+        &self,
+        player: GGPOPlayerHandle,
+        values: &dyn Any,
+        size: usize,
+    ) -> GGPOErrorCode {
+        if !self.running.get() {
+            return GGPOErrorCode::GGPO_ERRORCODE_NOT_SYNCHRONIZED;
+        }
+        let idx = player as usize;
+        for i in 0..size {
+            //let mut input = self.current_input.get_mut();
+            // TODO make this work
+            //input.bits[idx * size + i] |= (values as &[u8])[i];
+        }
+        GGPO_OK
+    }
+
+    fn sync_input(
+        &mut self,
+        values: &dyn Any,
+        size: usize,
+        disconnect_flags: &mut i32,
+    ) -> GGPOErrorCode {
+        let old_input = if self.rollingback.get() {
+            // TODO return head
+            //let (_, mut cons) = self.saved_frames.split();
+            //cons.head().input.clone()
+            todo!();
+        } else {
+            if self.sync.get_frame_count() == 0 {
+                self.sync.save_current_frame();
+            }
+            self.current_input.get_mut().clone()
+        };
+        let last_input = GameInput {
+            // TODO fix this
+            //bits: values as [u8; 18],
+            ..old_input
+        };
+        self.last_input.set(last_input);
+        if *disconnect_flags != 0 {
+            *disconnect_flags = 0i32;
+        }
+        GGPO_OK
+    }
+
+    fn increment_frame(&mut self) -> GGPOErrorCode {
+        self.sync.increment_frame();
+        self.current_input.get_mut().erase();
+
+        if self.rollingback.get() {
+            return GGPO_OK;
+        }
+        let frame = self.sync.get_frame_count();
+        let last_saved_frame = self.sync.get_last_saved_frame();
+        let saved_info = SavedInfo {
+            frame,
+            input: self.last_input.get_mut().clone(),
+            buf: last_saved_frame.buf.clone(),
+            ..*last_saved_frame
+        };
+        let a: &mut RingBuffer<SavedInfo> = self.saved_frames.get_mut();
+        // TODO fix this
+        let (b, _) = a.split();
+        if frame - self.last_verified.get() == self.check_distance {
+            self.sync.load_frame(self.last_verified.get());
+            self.rollingback.set(true);
+            let mut size = 3;
+            while size > 0 {
+                self.cb.advance_frame(0);
+
+                let mut info = self
+                    .saved_frames
+                    .get_mut()
+                    .split()
+                    .1
+                    .pop()
+                    .expect("Buffer was empty");
+                if info.frame != self.sync.get_frame_count() {
+                    panic!(
+                        "Frame number {} does not match saved frame number {}",
+                        info.frame, frame
+                    );
+                }
+
+                let checksum = self.sync.get_last_saved_frame().checksum;
+                if info.checksum != checksum {
+                    panic!(
+                        "Checksum for frame {} does not match saved ({} != {})",
+                        frame, checksum, info.checksum
+                    );
+                }
+                info.buf.clear();
+                size = 5;
+            }
+            self.last_verified.set(frame);
+            self.rollingback.set(false);
+        }
+        GGPO_OK
+    }
+}
+
+impl GGPOSession for SyncTestSession {}
+
+/////////////////////////
+/// P2P
+/////////////////////////
+
+struct P2PSession {
+    cb: Box<dyn GGPOSessionCallbacks>,
+    num_players: i32,
+    input_size: i32,
+    game_name: String,
+    local_port: u8,
+}
+
+impl Drop for P2PSession {
+    fn drop(&mut self) {}
+}
+
+impl opaque::Opaque for P2PSession {
+    fn add_player(
+        &self,
+        player: &GGPOPlayer,
+        handle: &GGPOPlayerHandle,
+    ) -> GGPOResult<GGPOPlayerHandle> {
+        Ok(0)
+    }
+    fn add_local_input(
+        &self,
+        player: GGPOPlayerHandle,
+        values: &dyn Any,
+        size: usize,
+    ) -> GGPOErrorCode {
+        GGPO_OK
+    }
+    fn sync_input(
+        &mut self,
+        values: &dyn Any,
+        size: usize,
+        disconnect_flags: &mut i32,
+    ) -> GGPOErrorCode {
+        GGPO_OK
+    }
+}
+
+impl GGPOSession for P2PSession {}
+
+/////////////////////////
+/// Spectator
+/////////////////////////
+
+struct SpectatorSession {
+    cb: Box<dyn GGPOSessionCallbacks>,
+    num_players: i32,
+    game_name: String,
+    local_port: u8,
+    input_size: i32,
+    host_ip: String,
+    host_port: u8,
+}
+
+impl Drop for SpectatorSession {
+    fn drop(&mut self) {}
+}
+
+impl opaque::Opaque for SpectatorSession {
+    fn add_player(
+        &self,
+        player: &GGPOPlayer,
+        handle: &GGPOPlayerHandle,
+    ) -> GGPOResult<GGPOPlayerHandle> {
+        Ok(0)
+    }
+    fn add_local_input(
+        &self,
+        player: GGPOPlayerHandle,
+        values: &dyn Any,
+        size: usize,
+    ) -> GGPOErrorCode {
+        GGPO_OK
+    }
+    fn sync_input(
+        &mut self,
+        values: &dyn Any,
+        size: usize,
+        disconnect_flags: &mut i32,
+    ) -> GGPOErrorCode {
+        GGPO_OK
+    }
+}
+
+impl GGPOSession for SpectatorSession {}
+
+/////////////////////////
+/// Modules
+/////////////////////////
 
 mod opaque {
     use super::*;
@@ -26,10 +319,15 @@ mod opaque {
             &self,
             player: GGPOPlayerHandle,
             values: &dyn Any,
-            size: i32,
+            size: usize,
         ) -> GGPOErrorCode;
-        fn sync_input(&self, values: &dyn Any, size: i32, disconnect_flags: &i32) -> GGPOErrorCode;
-        fn increment_frame(&self) -> GGPOErrorCode {
+        fn sync_input(
+            &mut self,
+            values: &dyn Any,
+            size: usize,
+            disconnect_flags: &mut i32,
+        ) -> GGPOErrorCode;
+        fn increment_frame(&mut self) -> GGPOErrorCode {
             GGPO_OK
         }
         fn chat(&self, text: &str) -> GGPOErrorCode {
@@ -58,111 +356,9 @@ mod opaque {
 
 pub trait GGPOSession: opaque::Opaque {}
 
-struct P2PSession {
-    cb: Box<dyn GGPOSessionCallbacks>,
-    num_players: i32,
-    input_size: i32,
-    game_name: String,
-    local_port: u8,
-}
-
-impl Drop for P2PSession {
-    fn drop(&mut self) {}
-}
-
-impl opaque::Opaque for P2PSession {
-    fn add_player(
-        &self,
-        player: &GGPOPlayer,
-        handle: &GGPOPlayerHandle,
-    ) -> GGPOResult<GGPOPlayerHandle> {
-        Ok(0)
-    }
-    fn add_local_input(
-        &self,
-        player: GGPOPlayerHandle,
-        values: &dyn Any,
-        size: i32,
-    ) -> GGPOErrorCode {
-        GGPO_OK
-    }
-    fn sync_input(&self, values: &dyn Any, size: i32, disconnect_flags: &i32) -> GGPOErrorCode {
-        GGPO_OK
-    }
-}
-
-impl GGPOSession for P2PSession {}
-
-struct SyncTestSession {
-    cb: Box<dyn GGPOSessionCallbacks>,
-    num_players: i32,
-    game_name: String,
-    frames: i32,
-}
-
-impl Drop for SyncTestSession {
-    fn drop(&mut self) {}
-}
-
-impl opaque::Opaque for SyncTestSession {
-    fn add_player(
-        &self,
-        player: &GGPOPlayer,
-        handle: &GGPOPlayerHandle,
-    ) -> GGPOResult<GGPOPlayerHandle> {
-        Ok(0)
-    }
-    fn add_local_input(
-        &self,
-        player: GGPOPlayerHandle,
-        values: &dyn Any,
-        size: i32,
-    ) -> GGPOErrorCode {
-        GGPO_OK
-    }
-    fn sync_input(&self, values: &dyn Any, size: i32, disconnect_flags: &i32) -> GGPOErrorCode {
-        GGPO_OK
-    }
-}
-
-impl GGPOSession for SyncTestSession {}
-
-struct SpectatorSession {
-    cb: Box<dyn GGPOSessionCallbacks>,
-    num_players: i32,
-    game_name: String,
-    local_port: u8,
-    input_size: i32,
-    host_ip: String,
-    host_port: u8,
-}
-
-impl Drop for SpectatorSession {
-    fn drop(&mut self) {}
-}
-
-impl opaque::Opaque for SpectatorSession {
-    fn add_player(
-        &self,
-        player: &GGPOPlayer,
-        handle: &GGPOPlayerHandle,
-    ) -> GGPOResult<GGPOPlayerHandle> {
-        Ok(0)
-    }
-    fn add_local_input(
-        &self,
-        player: GGPOPlayerHandle,
-        values: &dyn Any,
-        size: i32,
-    ) -> GGPOErrorCode {
-        GGPO_OK
-    }
-    fn sync_input(&self, values: &dyn Any, size: i32, disconnect_flags: &i32) -> GGPOErrorCode {
-        GGPO_OK
-    }
-}
-
-impl GGPOSession for SpectatorSession {}
+/////////////////////////
+/// Types
+/////////////////////////
 
 pub type GGPOPlayerHandle = i32;
 
@@ -306,7 +502,10 @@ pub enum GGPONetworkStats {
     Timesync(GGPONetworkStatsTimesync),
 }
 
-// C-style API
+/////////////////////////
+/// C-style API
+/////////////////////////
+
 pub fn ggpo_start_session(
     cb: impl GGPOSessionCallbacks + 'static,
     game: &str,
@@ -338,12 +537,22 @@ pub fn ggpo_start_synctest(
     input_size: i32,
     frames: i32,
 ) -> GGPOResult<impl GGPOSession> {
-    Ok(SyncTestSession {
+    let sess = SyncTestSession {
         cb: Box::new(cb),
         num_players,
-        game_name: game.to_owned(),
-        frames,
-    })
+        game: game.to_owned(),
+        check_distance: frames,
+        last_verified: Cell::new(0),
+        rollingback: Cell::new(false),
+        running: Cell::new(false),
+        logfp: None,
+        sync: GGPOSync,
+        current_input: Cell::new(GameInput::default()),
+        last_input: Cell::new(GameInput::default()),
+        saved_frames: RefCell::new(RingBuffer::new(32)),
+    };
+    sess.cb.begin_game(game);
+    Ok(sess)
 }
 
 pub fn ggpo_start_spectating(
@@ -387,15 +596,15 @@ pub fn ggpo_add_local_input(
     session: &mut impl GGPOSession,
     player: GGPOPlayerHandle,
     values: &dyn Any,
-    size: i32,
+    size: usize,
 ) -> GGPOErrorCode {
     session.add_local_input(player, values, size)
 }
 pub fn ggpo_synchronize_input(
     session: &mut impl GGPOSession,
     values: &dyn Any,
-    size: i32,
-    disconnect_flags: &i32,
+    size: usize,
+    disconnect_flags: &mut i32,
 ) -> GGPOErrorCode {
     session.sync_input(values, size, disconnect_flags)
 }
